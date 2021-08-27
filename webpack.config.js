2bee81cfb023cbbdaa85527fd81728acfb69098e
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2021-08-27 18:54:33 +0200

    add a React client with Webpack serving

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+function resolvePath(...segments) {
+  return path.resolve(__dirname, ...segments)
+}
+
+const baseConfig = {
+  target: 'web',
+  entry: {
+    client: resolvePath('src/client/index.tsx')
+  },
+  output: {
+    path: resolvePath('dist/client'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: '/node_modules/',
+        include: resolvePath('src/client'),
+        use: 'ts-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: resolvePath('src/client/index.html')
+      }
+    )
+  ]
+}
+
+const prodConfig = {
+  mode: 'production'
+}
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    open: '/index.html',
+    hot: true
+  }
+}
+
+module.exports = (env) => {
+  const extraConf = env.dev === true ? devConfig : prodConfig
+  return merge(baseConfig, extraConf)
+}
